2c036c7121188040832b01a9b3966e3e7e7287f5
redes19 <120016236+redes19@users.noreply.github.com>
2024-01-26 16:17:40 +0100

    feat(view)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -15,11 +15,11 @@ function App() {
       <section class="ecran">
         <div class="rec1"></div>
         <div class="carré1"></div>
-        <div class="rec2"></div>
-        <div class="carré2"></div>
+        {/* <div class="rec2"></div>
+        <div class="carré2"></div> */}
         <div class="rec3"></div>
-        <div class="carré3"></div>
-        <div class="rec4"></div>
+        {/* <div class="carré3"></div> */}
+        {/* <div class="rec4"></div> */}
         <div class="carré4"></div>
         <div className="cadre">
           <div className="container-info">
@@ -38,14 +38,14 @@ function App() {
             <Sommaire />
           </div>
         </div>
-        <div class="rec5"></div>
-        <div class="carré5"></div>
-        <div class="rec6"></div>
+        {/* <div class="rec5"></div>
+        <div class="carré5"></div> */}
+        {/* <div class="rec6"></div> */}
         <div class="carré6"></div>
         <div class="rec7"></div>
         <div class="carré7"></div>
         <div class="rec8"></div>
-        <div class="carré8"></div>
+        {/* <div class="carré8"></div> */}
       </section>
     </div>
   );
