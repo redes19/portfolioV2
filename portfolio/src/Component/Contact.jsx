1c855e8a359180b2e9db057878a484c45b84a205
redes19 <120016236+redes19@users.noreply.github.com>
2024-05-10 10:43:28 +0200

    push logo

--- portfolio/src/Component/Contact.jsx
+++ portfolio/src/Component/Contact.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../CSS/contact.css';
 // import Insta from '../logo/instagram.png';
-import Github from '../logo/github.png';
+import Github from '../logo/github-logo.webp';
 import Linkedin from '../logo/linkedin.png';
-import Mail from '../logo/mail.png';
+import Mail from '../logo/mail-logo.png';
 
 export default function Contact() {
   const emailAddress = 'lloris19@outlook.fr';
@@ -12,7 +12,7 @@ export default function Contact() {
     <div className='contact-logo'>
       <ul>
         <li className='list-logo'> <a href="https://www.linkedin.com/in/loris-laurenti-ab4081260/" target="_blank" rel="noopener noreferrer"><img src={Linkedin} alt="Linkedin" /></a> </li>
-        <li className='list-logo'> <a href="https://github.com/redes19" target="_blank" rel="noopener noreferrer"><img src={Github} alt="Github" /></a> </li>
+        <li className='list-logo'> <a href="https://github.com/redes19" target="_blank" rel="noopener noreferrer"><img src={Github} alt="githubLogo.jpg" /></a> </li>
         <li className='list-logo'><a href={`mailto:${emailAddress}`} target="_blank" rel="noopener noreferrer"><img src={Mail} alt="Mail" /> </a> </li>
       </ul>
     </div>
