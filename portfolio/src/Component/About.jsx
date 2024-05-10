f0187a17fa208872e19ab420a6cb31fc3dc77e1b
redes19 <120016236+redes19@users.noreply.github.com>
2024-05-10 10:20:13 +0200

    .

--- portfolio/src/Component/About.jsx
+++ portfolio/src/Component/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
 
   return (
     <div className='about-text'>
-      <p>Je suis actuellement en formation pour devenir un développeur web, et mon objectif est d'en faire mon métier.<br/> J'ai une passion particulière pour le back-end, mais j'apprécie également le front-end. <br/><br/>Mes premiers pas dans le monde de la programmation ont été orientés vers les jeux vidéo, et bien que j'aie le projet de créer mes propres jeux à l'avenir, je préfère actuellement me concentrer sur la programmation web, qui me passionne énormément.</p>
+      <p>Je suis actuellement en formation pour devenir un développeur web, et mon objectif est d'en faire mon métier.<br/> J'aime tout aussi bien coder le front-end que le back-end <br/><br/>Mes premiers pas dans le monde de la programmation ont été orientés vers les jeux vidéo, et bien que j'aie le projet de créer mes propres jeux à l'avenir, je préfère actuellement me concentrer sur la programmation web, qui me passionne énormément.</p>
       <span><button onClick={handleDownload}>
       Télécharger le CV (PDF)
     </button></span>
